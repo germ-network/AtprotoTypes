17e1fcc01594717c3b9686c97e1d593cd477d3b3
Mark Xue <mark@germ.network>
2026-03-29 16:21:31 -0700

    add ProxyService

---
+++ Sources/AtprotoTypes/Lexicon/XRPC/ProxyService.swift
@@ -0,0 +1,23 @@
+//
+//  ProxyService.swift
+//  AtprotoTypes
+//
+//  Created by Mark @ Germ on 3/29/26.
+//
+
+import Foundation
+
+///https://atproto.com/specs/xrpc#service-proxying
+public struct ProxyService {
+	public let did: Atproto.DID
+	public let endpoint: String
+	
+	public init(did: Atproto.DID, endpoint: String) {
+		self.did = did
+		self.endpoint = endpoint
+	}
+	
+	public var headerValue: String {
+		did.stringRepresentation + "#" + endpoint
+	}
+}
