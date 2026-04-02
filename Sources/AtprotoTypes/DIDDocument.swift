74b4daea4682f2ac245f9a238f14079fda37ef6e
Mark Xue <mark@germ.network>
2026-04-02 00:28:15 -0700

    add public init for Atproto.Service

--- Sources/AtprotoTypes/DIDDocument.swift
+++ Sources/AtprotoTypes/DIDDocument.swift
@@ -134,6 +134,12 @@ extension Atproto {
 
 		/// The endpoint URL for the service, specifying the location of the service.
 		public let serviceEndpoint: URL
+		
+		public init(id: String, type: String, serviceEndpoint: URL) {
+			self.id = id
+			self.type = type
+			self.serviceEndpoint = serviceEndpoint
+		}
 	}
 }
 
