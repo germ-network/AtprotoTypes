36c5500959137da32d8ec8cbe9e00dfd4991725a
Anna M <anna@germ.network>
2026-04-02 18:14:19 -0700

    Add delete record

---
+++ .changeset/breezy-roses-argue.md
@@ -0,0 +1,5 @@
+---
+"@germ-network/atprototypes": minor
+---
+
+adding delete record
---
+++ Sources/AtprotoTypes/Lexicon/XRPC/Procedure/DeleteRecord.swift
@@ -0,0 +1,63 @@
+//
+//  DeleteRecord.swift
+//  AtprotoTypes
+//
+//  Created by Anna @ Germ on 4/2/26.
+//
+
+import Foundation
+import GermConvenience
+import HTTPTypes
+
+///https://docs.bsky.app/docs/api/com-atproto-repo-delete-record
+///https://lexicon.garden/lexicon/did:plc:6msi3pj7krzih5qxqtryxlzw/com.atproto.repo.deleteRecord/docs
+extension Lexicon.Com.Atproto.Repo {
+	public static let deleteRecordNSID: Atproto.NSID = "com.atproto.repo.deleteRecord"
+
+	public enum DeleteRecord<Record: AtprotoRecord>: XRPCProcedure {
+		public static var nsid: Atproto.NSID { deleteRecordNSID }
+		public static var outputEncoding: HTTPContentType { .json }
+
+		public struct Input: XRPCProcedureInput {
+			public static var encoding: HTTPContentType { .json }
+			public static func encode(_ schema: Schema) throws -> Data? {
+				try JSONEncoder().encode(schema)
+			}
+
+			public struct Schema: Encodable, Sendable {
+				let repo: AtIdentifier
+				let collection: Atproto.NSID
+				let rkey: Atproto.RecordKey?
+				let swapRecord: CID?
+				let swapCommit: CID?
+
+				public init(
+					repo: AtIdentifier,
+					rkey: Atproto.RecordKey?,
+					swapRecord: CID? = nil,
+					swapCommit: CID? = nil,
+				) {
+					self.repo = repo
+					self.collection = Record.nsid
+					self.rkey = rkey
+					self.swapRecord = swapRecord
+					self.swapCommit = swapCommit
+				}
+			}
+			public let schema: Schema
+
+			public init(schema: Schema) {
+				self.schema = schema
+			}
+		}
+
+		public typealias Parameters = EmptyXRPCParameters
+		public typealias Output = PutRecordResult
+	}
+}
+
+extension Lexicon.Com.Atproto.Repo.DeleteRecord: XRPCResponseParsing {
+	public static var badRequestErrors: Set<String> {
+		defaultErrors.union(["InvalidSwap"])
+	}
+}
