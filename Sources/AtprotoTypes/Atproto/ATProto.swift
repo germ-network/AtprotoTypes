b70adb53a5d429106d84c757e9166472e3a270fb
Mark Xue <mark@germ.network>
2026-04-14 01:03:21 -0700

    move record into its own file

--- Sources/AtprotoTypes/Atproto/ATProto.swift
+++ Sources/AtprotoTypes/Atproto/ATProto.swift
@@ -1,21 +1,4 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public enum Atproto {
-	public protocol Record: Sendable, Codable {
-		associatedtype Id: RecordId
-		associatedtype Key: RecordKey
-	}
-
-	//whereas NSID defines a structure, RecordId is a NSID used as
-	//a collection id
-	public protocol RecordId: FixedString {
-		static var nsid: NSID { get }
-		init()
-	}
-}
-
-//default implementations for FixedString and Codable
-extension Atproto.RecordId {
-	static public var fixedValue: String { nsid.rawValue }
-}
+public enum Atproto {}
